c2350470e9c8145bcf4c5c863cc50724ce35370a
InsertString <joshuacanadian@gmail.com>
2019-11-06 18:46:11 -0800

    2019-11-06 -cleaned up driveControlCode function

--- src/cube_system.cpp
+++ src/cube_system.cpp
@@ -20,29 +20,24 @@ void Cube_System::driveControlCode() {
 
   switch (stacking_steps) {
     case 1 :
-    if (elevators_to_position(BOT)) {
+    if (elevators_to_position(BOT))
       stacking_steps++;
-    }
     break;
     case 2 :
-    if (elevators_to_position(TOP)) {
+    if (elevators_to_position(TOP))
       stacking_steps = 0;
-    }
     break;
     case 0 :
     elevators_to_position(TOP);
     break;
   }
 
-  if (master.get_digital(DIGITAL_R1)) {
+  if (master.get_digital(DIGITAL_R1))
     set_intake_power(127);
-  }
-  else if (master.get_digital(DIGITAL_R2)) {
+  else if (master.get_digital(DIGITAL_R2))
     set_intake_power(-127);
-  }
-  else {
+  else
     set_intake_power(0);
-  }
 
 
 }
