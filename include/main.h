a3e006615563dc07921a5e898b3f015f4df6d662
InsertString <joshuacanadian@gmail.com>
2020-01-13 10:05:03 -0800

    2020-01-13 -changed to single meta bot code

--- include/main.h
+++ include/main.h
@@ -5,23 +5,18 @@
 
 #include "api.h"
 #include "PID.hpp"
-#include "cube_system.hpp"
 
 using namespace pros;
 
 
 
 // motor ports
-#define DRIVE_LEFT_FT 1
-#define DRIVE_LEFT_FB 2
-#define DRIVE_LEFT_B 3
-#define DRIVE_RIGHT_FT 4
-#define DRIVE_RIGHT_FB 5
-#define DRIVE_RIGHT_B 6
-#define INTAKE_L 7
-#define INTAKE_R 8
-#define ELEVATOR_L 9
-#define ELEVATOR_R 10
+#define DRIVE_LEFT_F 2
+#define DRIVE_LEFT_B 12
+#define DRIVE_RIGHT_F 1
+#define DRIVE_RIGHT_B 11
+#define INTAKE_LEFT 4
+#define INTAKE_RIGHT 6
 
 // sensor ports
 
@@ -42,28 +37,5 @@ void opcontrol(void);
 
 #ifdef __cplusplus
 #endif
-
-extern Controller master;
-
-extern Motor drive_left_FT;
-extern Motor drive_left_FB;
-extern Motor drive_left_B;
-
-extern Motor drive_right_FT;
-extern Motor drive_right_FB;
-extern Motor drive_right_B;
-
-extern Motor intake_L;
-extern Motor intake_R;
-
-extern Motor elevator_L;
-extern Motor elevator_R;
-
-extern ADIDigitalIn elevator_limit_L;
-extern ADIDigitalIn elevator_limit_R;
-
-extern ADILineSensor cube_detector_front;
-//extern ADILineSensor cube_detector_front;
-extern ADIDigitalIn cube_detector_back;
 
 #endif  // _PROS_MAIN_H_
--- src/main.cpp
+++ src/main.cpp
@@ -3,48 +3,32 @@
 // controller definition
 Controller master(E_CONTROLLER_MASTER);
 
+
 // motor definition
-Motor drive_left_FT(DRIVE_LEFT_FT);
-Motor drive_left_FB(DRIVE_LEFT_FB);
+Motor drive_left_F(DRIVE_LEFT_F);
+
 Motor drive_left_B(DRIVE_LEFT_B);
 
-Motor drive_right_FT(DRIVE_RIGHT_FT);
-Motor drive_right_FB(DRIVE_RIGHT_FB);
+Motor drive_right_F(DRIVE_RIGHT_F);
+
 Motor drive_right_B(DRIVE_RIGHT_B);
 
-Motor intake_L(INTAKE_L);
-Motor intake_R(INTAKE_R);
+Motor intake_left(INTAKE_LEFT);
+Motor intake_right(INTAKE_RIGHT);
 
-Motor elevator_L(ELEVATOR_L);
-Motor elevator_R(ELEVATOR_R);
-
-
-// sensors
-
-// these limits are placed at the top of each elevator
-// they help keep encoder values consistant throughout the match
-
-ADIDigitalIn elevator_limit_L(1);
-ADIDigitalIn elevator_limit_R(2);
-
-// lne sensors detect when cubes are in different parts
-// of the intake for the autostack algorithum
-
-ADILineSensor cube_detector_front(3);
-//ADILineSensor cube_detector_front(4);
-ADIDigitalIn cube_detector_back(4);
-
-
-// classes
-
-Cube_System cube_system;
-
+ADIDigitalIn limit(1);
 
 
 void initialize() {
+
+
 	lcd::initialize();
-	intake_L.tare_position();
-	intake_R.tare_position();
+
+	drive_left_B.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	drive_left_F.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	drive_right_B.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	drive_right_F.set_brake_mode(MOTOR_BRAKE_BRAKE);
+
 }
 
 
@@ -63,41 +47,87 @@ void competition_initialize() {}
 void autonomous() {}
 
 
-
+#define TRAY 1
+#define DRIVE 0
 
 void opcontrol() {
-	int left = 0;
-	int right = 0;
+	int moving_state = DRIVE;
+	int tray_moved = 0;
 	while (true) {
-		lcd::print(0, "cube light val %f", cube_system.front_light_value());
 
-		cube_system.driveControlCode();
-
-		left = master.get_analog(ANALOG_LEFT_Y);
-		right = -master.get_analog(ANALOG_RIGHT_Y);
-
-
-		drive_left_B = left;
-		drive_left_FB = left;
-		drive_left_FT = -left;
-
-		drive_right_B = right;
-		drive_right_FB = right;
-		drive_right_FT = -right;
-
-
-		if (master.get_digital(DIGITAL_L1)) {
-			elevator_L = 127;
-			elevator_R = 127;
+		if (limit.get_value() == 1 && moving_state == DRIVE) {
+			drive_left_F.tare_position();
+			drive_left_B.tare_position();
+			drive_right_B.tare_position();
+			drive_right_F.tare_position();
 		}
-		else if (master.get_digital(DIGITAL_L2)) {
-			elevator_L = -127;
-			elevator_R = -127;
+
+
+
+		if (master.get_digital(DIGITAL_R1)) {
+			intake_left = 127;
+			intake_right = -127;
+		}
+		else if (master.get_digital(DIGITAL_R2)) {
+			intake_left = -70;
+			intake_right = 70;
 		}
 		else {
-			elevator_L = 0;
-			elevator_R = 0;
+			intake_left = 0;
+			intake_right = 0;
 		}
+
+
+
+		if (abs(master.get_analog(ANALOG_LEFT_Y)) > 20 || abs(master.get_analog(ANALOG_RIGHT_Y)) > 20) {
+			moving_state = DRIVE;
+		}
+		else {
+			moving_state = TRAY;
+		}
+
+
+
+		if (moving_state == DRIVE) {
+			drive_left_B = -master.get_analog(ANALOG_LEFT_Y);
+			drive_left_F = master.get_analog(ANALOG_LEFT_Y);
+
+			drive_right_B = master.get_analog(ANALOG_RIGHT_Y);
+			drive_right_F = -master.get_analog(ANALOG_RIGHT_Y);
+		}
+		else if (moving_state == TRAY) {
+			if (master.get_digital(DIGITAL_L1)) {
+				drive_left_B.move_absolute(500, 30);
+				drive_left_F.move_absolute(500, 30);
+
+				drive_right_B.move_absolute(-500, 30);
+				drive_right_F.move_absolute(-500, 30);
+			}
+			else if (master.get_digital(DIGITAL_UP)) {
+
+				drive_left_B = 30;
+				drive_left_F = 30;
+
+				drive_right_B = -30;
+				drive_right_F = -30;
+			}
+			else if (master.get_digital(DIGITAL_L2)) {
+				drive_left_B = -50;
+				drive_left_F = -50;
+
+				drive_right_B = 50;
+				drive_right_F = 50;
+				tray_moved = 0;
+			}
+			else {
+				drive_left_B = 0;
+				drive_left_F = 0;
+
+				drive_right_B = 0;
+				drive_right_F = 0;
+			}
+		}
+
 
 		delay(20);
 	}
